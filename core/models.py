568fcf3c8fb114ee553c507b00612ce5d45399bb
Amanda Phillipson <amanda.minton@gmail.com>
2019-11-14 09:11:26 -0500

    Start of new website for Geoff Moore Inc

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+# class Comment(models.Model):
+# 	“”” Create a doc string to represent instructions for what the class does”””
+# 	text = models.TextField()
+# 	def __str__(self):
+# 	“””override the way Django returns the information in a string”””
+# 		return self.text
