f305d15b6480d4757c6ac5860a86f27be433cbf0
Amanda Phillipson <amanda.minton@gmail.com>
2019-12-13 10:20:29 -0500

    core, settings, urls

--- core/apps.py
+++ core/apps.py
@@ -6,3 +6,12 @@ class CoreConfig(AppConfig):
 
 ‘core.apps.CoreConfig’
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'catalog', 
+]
